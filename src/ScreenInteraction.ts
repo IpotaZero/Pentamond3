c2c7290696c561ccb1deaba9546e3f3184a7dae8
ipota <kiratoki512810@gmail.com>
2026-01-24 19:41:11 +0900

    fix: ScreenInteractionを軽くリファクタリング

--- src/ScreenInteraction.ts
+++ src/ScreenInteraction.ts
@@ -1,5 +1,5 @@
 import { EventId, EventManager, MyEventListener } from "./EventManager";
-import { Input } from "./Interaction/Input";
+import { Input, OperateManager } from "./Interaction/Input";
 import { inputManager } from "./Interaction/InputManager";
 import { pageManager } from "./PageManager";
 import { sleep, qs, qsAll, getMinElement, removeMousePointerTemporary, qsAddEvent } from "./Utils";
@@ -18,7 +18,8 @@ class ScreenInteraction implements MyEventListener {
     private operable: boolean = true;
     private focusFlag = false;
     private lastOperateTime = 0;
-    private mappedElements: MappedElement[] = [];
+    /**現在のページのMappedElements */
+    private currentMappedElements: MappedElement[] = [];
     private static instance: ScreenInteraction;
 
     /**
@@ -59,53 +60,16 @@ class ScreenInteraction implements MyEventListener {
     }
 
     get g$selectedElement(): MappedElement | null {
-        return this.mappedElements.find(({ element: option }) => option == document.activeElement) ?? null;
+        return this.currentMappedElements.find(({ element: option }) => option == document.activeElement) ?? null;
     }
 
     private addInteractionEvents() {
         //ページ変更ごとにそのページを操作できるイベントをinputに追加する
-        pageManager.addEvent(["pageChanged"], async () => {
-            const page = pageManager.g$currentPage;
-            if (!page) {
-                return;
-            }
-            const id = page.id;
-            if (document.activeElement instanceof HTMLElement) {
-                document.activeElement.blur();
-            }
-            //情報を更新する
-            EventManager.removeEvents(this.pageOperateEvents);
-            this.pageOperateEvents.length = 0;
-            this.listeningInputs.length = 0;
-            //ページのボタンを最初にfocusするか
-            if (this.focusFlag) {
-                qs(`#${id} [data-mapping]`)?.focus();
-                this.focusFlag = false;
-            }
-            //早すぎるページ遷移の禁止
-            await sleep(Setting.debounceOperateTime * 2.5);
-            //イベントの追加
-            inputManager.g$inputs.forEach((input) => {
-                if (input.isAuto()) {
-                    return;
-                }
-                this.addPageOperateEvent(id, input);
-            });
-            //mappingが設定されたElementを取得
-            this.setMappedElements(id);
+        pageManager.addEvent(["pageChanged"], () => {
+            this.onPageChanged();
         });
 
-        //マウス操作との整合
-        qsAll("[data-mapping]")
-            .filter((e) => e instanceof HTMLElement)
-            .forEach((button) => {
-                this.setHoverHighlight(button);
-            });
-
-        //最後にclickを行った時間の更新
-        qsAddEvent("[data-mapping]", "click", () => {
-            this.lastOperateTime = Date.now();
-        });
+        this.setupDataMapping();
 
         //途中追加されたinputにイベントの追加
         inputManager.addEvent(["inputAdded"], () => {
@@ -115,6 +79,15 @@ class ScreenInteraction implements MyEventListener {
             }
             this.addPageOperateEvent(pageManager.g$currentPageId ?? "pageStart", latestInput);
         });
+    }
+
+    private setupDataMapping() {
+        //マウス操作との整合
+        qsAll("[data-mapping]")
+            .filter((e) => e instanceof HTMLElement)
+            .forEach((button) => {
+                this.setHoverHighlight(button);
+            });
 
         //input要素を触った後にそれを含む要素にfocusさせる
         qsAll(".rangeContainer[data-mapping]").forEach((container) => {
@@ -124,86 +97,168 @@ class ScreenInteraction implements MyEventListener {
                 });
             });
         });
+
+        //最後にclickを行った時間の更新
+        qsAddEvent("[data-mapping]", "click", () => {
+            this.lastOperateTime = Date.now();
+        });
+    }
+
+    private async onPageChanged() {
+        const id = pageManager.g$currentPageId;
+        if (!id) return;
+
+        this.shiftFocusElement(id);
+
+        //情報を更新する
+        EventManager.removeEvents(this.pageOperateEvents);
+        this.pageOperateEvents.length = 0;
+        this.listeningInputs.length = 0;
+
+        //早すぎるページ遷移の禁止
+        await sleep(Setting.debounceOperateTime * 2.5);
+
+        //イベントの追加
+        inputManager.g$inputs.forEach((input) => {
+            if (input.isAuto()) return;
+
+            this.addPageOperateEvent(id, input);
+        });
+
+        //mappingが設定されたElementを取得
+        this.currentMappedElements = this.createMappedElements(id);
+    }
+
+    private shiftFocusElement(id: string) {
+        if (document.activeElement instanceof HTMLElement) {
+            document.activeElement.blur();
+        }
+
+        //ページのボタンを最初にfocusするか
+        if (this.focusFlag) {
+            qs(`#${id} [data-mapping]`)?.focus();
+            this.focusFlag = false;
+        }
     }
 
     /**
      * 指定したページを操作するEventを指定したinputに追加する
-     * @param id ページのid
+     * @param pageId ページのid
      * @param input Eventを追加するinput
      */
-    private addPageOperateEvent(id: string, input: Input): void {
+    private addPageOperateEvent(pageId: string, input: Input): void {
         if (this.listeningInputs.includes(input)) {
             return;
         }
+
         const manager = input.g$manager;
+
         const eventId = manager.addEvent(["onKeydown", "onButtondown", "onStickActive"], () => {
-            //前回の操作から間が空いていないならreturn
-            if (Date.now() - this.lastOperateTime <= Setting.debounceOperateTime || !this.operable) {
-                return;
-            }
-            const latestKey = manager.g$latestPressingKey;
-            if (["KeyX", "Escape", "Backspace", "button:0"].includes(latestKey)) {
-                this.focusFlag = true;
-                const backElement = qs(`#${id} .back`);
-                if (!backElement) {
-                    EventManager.executeEventsByClassName("interaction");
-                    return;
-                }
-                this.updateLastOperateTime();
-                EventManager.executeEventsByClassName("interaction");
-                EventManager.executeEventsByClassName("cancelInteraction");
-                backElement.click();
-                return;
-            }
-            if (!this.g$selectedElement) {
-                if (this.mappedElements.length) {
-                    this.mappedElements[0].element.focus();
-                }
-                EventManager.executeEventsByClassName("interaction");
-                return;
-            }
-            if (["ArrowUp", "KeyW", "button:12", "stick:-1"].includes(latestKey)) {
-                const element = this.getMoveElement([null, -1]);
-                element.focus();
-                this.updateLastOperateTime();
-            }
-            if (["ArrowDown", "KeyS", "button:13", "stick:+1"].includes(latestKey)) {
-                const element = this.getMoveElement([null, 1]);
-                element.focus();
-                this.updateLastOperateTime();
-            }
-            if (["ArrowLeft", "KeyA", "button:14", "stick:-0"].includes(latestKey)) {
-                const element = this.getMoveElement([-1, null]);
-                if (element.classList.contains("rangeContainer")) {
-                    const input = element.querySelector<HTMLInputElement>("input")!;
-                    input.stepDown();
-                    input.dispatchEvent(new Event("input"));
-                }
-                element.focus();
-                this.updateLastOperateTime();
-            }
-            if (["ArrowRight", "KeyD", "button:15", "stick:+0"].includes(latestKey)) {
-                const element = this.getMoveElement([1, null]);
-                if (element.classList.contains("rangeContainer")) {
-                    const input = element.querySelector<HTMLInputElement>("input")!;
-                    input.stepUp();
-                    input.dispatchEvent(new Event("input"));
-                }
-                element.focus();
-                this.updateLastOperateTime();
-            }
-            if (["Enter", "Space", "KeyZ", "button:1"].includes(latestKey)) {
-                if (document.activeElement instanceof HTMLElement) {
-                    this.focusFlag = true;
-                    this.g$selectedElement.element.click();
-                    this.updateLastOperateTime();
-                    EventManager.executeEventsByClassName("confirmInteraction");
-                }
-            }
-            EventManager.executeEventsByClassName("interaction");
+            this.onInput(pageId, manager.g$latestPressingKey);
         });
         this.pageOperateEvents.push(eventId);
         this.listeningInputs.push(input);
+    }
+
+    private onFirstOperation() {
+        this.currentMappedElements[0]?.element.focus();
+        EventManager.executeEventsByClassName("interaction");
+    }
+
+    private onCancel(pageId: string) {
+        this.focusFlag = true;
+
+        const backElement = qs(`#${pageId} .back`);
+
+        if (!backElement) {
+            EventManager.executeEventsByClassName("interaction");
+            return;
+        }
+
+        this.updateLastOperateTime();
+
+        EventManager.executeEventsByClassName("interaction");
+        EventManager.executeEventsByClassName("cancelInteraction");
+
+        backElement.click();
+    }
+
+    private onInput(pageId: string, latestKey: string) {
+        if (!this.operable) {
+            return;
+        }
+
+        //前回の操作から間が空いていないならreturn
+        if (Date.now() - this.lastOperateTime <= Setting.debounceOperateTime) {
+            return;
+        }
+
+        // まだどこにもフォーカスがない場合
+        if (!this.g$selectedElement) {
+            this.onFirstOperation();
+            return;
+        }
+
+        const cancelKeys = ["KeyX", "Escape", "Backspace", "button:0"];
+        const upKeys = ["ArrowUp", "KeyW", "button:12", "stick:-1"];
+        const downKeys = ["ArrowDown", "KeyS", "button:13", "stick:+1"];
+        const leftKeys = ["ArrowLeft", "KeyA", "button:14", "stick:-0"];
+        const rightKeys = ["ArrowRight", "KeyD", "button:15", "stick:+0"];
+        const okKeys = ["Enter", "Space", "KeyZ", "button:1"];
+
+        if (cancelKeys.includes(latestKey)) {
+            this.onCancel(pageId);
+            return;
+        }
+
+        if (upKeys.includes(latestKey)) {
+            const element = this.getTargetElement([null, -1]);
+            element.focus();
+            this.updateLastOperateTime();
+        }
+
+        if (downKeys.includes(latestKey)) {
+            const element = this.getTargetElement([null, 1]);
+            element.focus();
+            this.updateLastOperateTime();
+        }
+
+        if (leftKeys.includes(latestKey)) {
+            const element = this.getTargetElement([-1, null]);
+
+            if (element.classList.contains("rangeContainer")) {
+                const input = element.querySelector<HTMLInputElement>("input")!;
+                input.stepDown();
+                input.dispatchEvent(new Event("input"));
+            }
+
+            element.focus();
+            this.updateLastOperateTime();
+        }
+
+        if (rightKeys.includes(latestKey)) {
+            const element = this.getTargetElement([1, null]);
+
+            if (element.classList.contains("rangeContainer")) {
+                const input = element.querySelector<HTMLInputElement>("input")!;
+                input.stepUp();
+                input.dispatchEvent(new Event("input"));
+            }
+
+            element.focus();
+            this.updateLastOperateTime();
+        }
+
+        if (okKeys.includes(latestKey)) {
+            if (document.activeElement instanceof HTMLElement) {
+                this.focusFlag = true;
+                this.g$selectedElement.element.click();
+                this.updateLastOperateTime();
+                EventManager.executeEventsByClassName("confirmInteraction");
+            }
+        }
+
+        EventManager.executeEventsByClassName("interaction");
     }
 
     /**
@@ -228,17 +283,15 @@ class ScreenInteraction implements MyEventListener {
 
     /**
      * 指定したページのmappingが設定されている要素を取得する
-     * @param id ページのid
+     * @param pageId ページのid
      */
-    private setMappedElements(id: string) {
-        const elements = qsAll(`#${id} [data-mapping]`);
-        this.mappedElements = elements.map(
-            (element) =>
-                ({
-                    element: element,
-                    coordinate: JSON.parse(element.dataset.mapping!),
-                }) as MappedElement
-        );
+    private createMappedElements(pageId: string): MappedElement[] {
+        const elements = qsAll(`#${pageId} [data-mapping]`);
+
+        return elements.map((element) => ({
+            element: element,
+            coordinate: JSON.parse(element.dataset.mapping!) as [number, number],
+        }));
     }
 
     /**
@@ -247,28 +300,38 @@ class ScreenInteraction implements MyEventListener {
      * @param param0 相対的な移動先
      * @returns 移動先のHTMLElement
      */
-    private getMoveElement([dx, dy]: [number | null, number | null]): HTMLElement {
+    private getTargetElement([dx, dy]: [number | null, number | null]): HTMLElement {
         const selectedElement = this.g$selectedElement;
+
         if (!selectedElement) {
-            return this.mappedElements.length ? this.mappedElements[0].element : document.body;
+            if (this.currentMappedElements.length > 0) {
+                return this.currentMappedElements[0].element;
+            } else {
+                return document.body;
+            }
         }
+
         let [sx, sy] = selectedElement.coordinate;
-        let move = getMinElement(
-            this.mappedElements
+
+        let target = getMinElement(
+            this.currentMappedElements
                 .filter(({ coordinate: [x, y] }) => x == sx + (dx ?? x - sx) && y == sy + (dy ?? y - sy))
                 .map(({ element: option, coordinate: [x, y] }) => ({ element: option, value: Math.hypot(x - sx, y - sy) }))
         );
-        if (!move) {
-            move = getMinElement(
-                this.mappedElements
+
+        if (!target) {
+            target = getMinElement(
+                this.currentMappedElements
                     .filter(({ coordinate: [x, y] }) => x == (dx ? x : sx) && y == (dy ? y : sy))
                     .map(({ element: option, coordinate: [x, y] }) => ({ element: option, value: (dx ? 0 : Math.sign(dy ?? 0) * y) + (dy ? 0 : Math.sign(dx ?? 0) * x) }))
             );
         }
-        if (!move) {
-            move = selectedElement.element;
+
+        if (!target) {
+            target = selectedElement.element;
         }
-        return move;
+
+        return target;
     }
 }
 
